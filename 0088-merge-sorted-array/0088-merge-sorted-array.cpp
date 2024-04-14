2411fa1a9d71c7743dfc17a5efe94cf3250c0f31
BHOOSHAN D <151170193+bhooshan123@users.noreply.github.com>
2024-04-14 09:12:41 +0530

    Time: 0 ms (100%), Space: 10.9 MB (72.22%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+         nums1.resize(m + n);
+
+        copy(nums2.begin(), nums2.end(), nums1.begin() + m);
+
+        sort(nums1.begin(), nums1.end());
+        
+    }
+};
