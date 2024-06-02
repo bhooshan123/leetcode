d42193ad28fd68087ab7e5ad6c99dfae4901a059
BHOOSHAN D <151170193+bhooshan123@users.noreply.github.com>
2024-06-02 12:20:11 +0530

    Time: 17 ms (57.81%), Space: 26.9 MB (89.96%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        reverse(s.begin(),s.end());
+        int l=0,r=s.size()-1;
+        while(l<r){
+            swap(s[l],s[r]);
+            l++;
+            r--;
+        }
     }
 };
