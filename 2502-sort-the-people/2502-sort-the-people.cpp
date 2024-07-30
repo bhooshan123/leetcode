1d2506aa2c2b35a04ba5a7d1ba4951667c254397
BHOOSHAN D <151170193+bhooshan123@users.noreply.github.com>
2024-07-30 08:46:35 +0530

    Time: 29 ms (52.01%), Space: 25.8 MB (71.68%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.cpp
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        int n=names.size();
+        /*int n=names.size();
         vector<pair<int, string>> hn(n);
         for(int i=0; i<n; i++) 
             hn[i]={heights[i], names[i]};
         sort(hn.begin(), hn.end(), greater<>());
         for(int i=0; i<n; i++) 
             names[i]=hn[i].second;
-        return names;    
+        return names;  */
+        unordered_map<int, string> map;
+        for (int i = 0; i < names.size(); i++)
+            map[heights[i]] = names[i];
+        sort(heights.begin(), heights.end(), greater<int>());
+        for (int i = 0; i < names.size(); i++)
+            names[i] = map[heights[i]];
+        return names;  
     
     }
 };
